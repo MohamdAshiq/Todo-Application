29bd4473ee9b5f3ca47029f0181a7417ca2a328b
Mohammed Ashiq <mohammedashiq836@gmail.com>
2023-10-28 11:17:39 +0530

    Added Appbar and Floating Action Button

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -5,6 +5,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Todo Application",
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+        ),
+        elevation: 0.8,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        enableFeedback: false,
+        
+        backgroundColor: Colors.grey[900],
+        splashColor: Colors.grey[900],
+        icon: const Icon(Icons.add),
+        onPressed: () {},
+        label: const Text(
+          "Add Task",
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+        ),
+      ),
+    );
   }
 }
