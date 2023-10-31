6b805729b57e777032979722905108d7d392bd0b
Mohammed Ashiq <mohammedashiq836@gmail.com>
2023-10-31 14:23:37 +0530

    Added Category Section and assigned dummy datas

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_application/Widgets/custom_appbar.dart';
 import 'package:todo_application/Widgets/floating_action_button_widget.dart';
 import 'package:todo_application/responsive_layout.dart';
@@ -8,13 +9,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ResponsiveLayout(
-      desktopScaffold: Scaffold(),
-      tabletScaffold: Scaffold(),
+    return ResponsiveLayout(
+      desktopScaffold: const Scaffold(),
+      tabletScaffold: const Scaffold(),
       mobileScaffold: Scaffold(
-        appBar: CustomAppbar(),
+        appBar: const CustomAppbar(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButtonWidget(),
+        floatingActionButton: const FloatingActionButtonWidget(),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.h,
+            vertical: 15.h,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10.h,
+                ),
+                child: Text(
+                  "Category",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18.sp,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100.h,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.all(8.0.h),
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
+                          width: 50.w,
+                          height: 50.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.r),
+                            ),
+                            color: Colors.grey[700],
+                          ),
+                          child: const Icon(Icons.category),
+                        ),
+                        Text(
+                          "Data ${index + 1}",
+                          style: TextStyle(
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
